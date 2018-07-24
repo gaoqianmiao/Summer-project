0ea9078fb48c2fed48bc9395e5ff4f92310bac49
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-07-24 13:53:07 -0500

    Update recurrent_networks.py

--- FallDetectionSystem/model/recurrent_networks.py
+++ FallDetectionSystem/model/recurrent_networks.py
@@ -1,67 +1,250 @@
-from __future__ import absolute_import
+from keras import backend as K, initializers, regularizers, constraints
 
-import keras
-from keras.layers import LSTM, activations
+from keras.engine.topology import Layer
 
-class AttentionLSTM(LSTM):
-    def __init__(self, output_dim, attention_vec, attn_activation='tanh',
-                 attn_inner_activation='tanh', single_attn=False,
-                 n_attention_dim=None, **kwargs):
-        self.attention_vec = attention_vec
-        self.attn_activation = activations.get(attn_activation)
-        self.attn_inner_activation = activations.get(attn_inner_activation)
-        self.single_attention_param = single_attn
-        self.n_attention_dim = output_dim if n_attention_dim is None else n_attention_dim
 
-        super(AttentionLSTM, self).__init__(output_dim, **kwargs)
+
+
+
+def dot_product(x, kernel):
+
+    """
+
+    Wrapper for dot product operation, in order to be compatible with both
+
+    Theano and Tensorflow
+
+    Args:
+
+        x (): input
+
+        kernel (): weights
+
+    Returns:
+
+    """
+
+    if K.backend() == 'tensorflow':
+
+        # todo: check that this is correct
+
+        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
+
+    else:
+
+        return K.dot(x, kernel)
+
+
+
+
+
+class Attention(Layer):
+
+    def __init__(self,
+
+                 W_regularizer=None, b_regularizer=None,
+
+                 W_constraint=None, b_constraint=None,
+
+                 bias=True,
+
+                 return_attention=False,
+
+                 **kwargs):
+
+        """
+
+        Keras Layer that implements an Attention mechanism for temporal data.
+
+        Supports Masking.
+
+        Follows the work of Raffel et al. [https://arxiv.org/abs/1512.08756]
+
+        # Input shape
+
+            3D tensor with shape: `(samples, steps, features)`.
+
+        # Output shape
+
+            2D tensor with shape: `(samples, features)`.
+
+        :param kwargs:
+
+        Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+
+        The dimensions are inferred based on the output shape of the RNN.
+
+
+
+
+
+        Note: The layer has been tested with Keras 1.x
+
+
+
+        Example:
+
+        
+
+            # 1
+
+            model.add(LSTM(64, return_sequences=True))
+
+            model.add(Attention())
+
+            # next add a Dense layer (for classification/regression) or whatever...
+
+
+
+            # 2 - Get the attention scores
+
+            hidden = LSTM(64, return_sequences=True)(words)
+
+            sentence, word_scores = Attention(return_attention=True)(hidden)
+
+
+
+        """
+
+        self.supports_masking = True
+
+        self.return_attention = return_attention
+
+        self.init = initializers.get('glorot_uniform')
+
+
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+
+
+        self.W_constraint = constraints.get(W_constraint)
+
+        self.b_constraint = constraints.get(b_constraint)
+
+
+
+        self.bias = bias
+
+        super(Attention, self).__init__(**kwargs)
+
+
 
     def build(self, input_shape):
-        super(AttentionLSTM, self).build(input_shape)
 
-        if hasattr(self.attention_vec, '_keras_shape'):
-            attention_dim = self.attention_vec._keras_shape[1]
+        assert len(input_shape) == 3
+
+
+
+        self.W = self.add_weight((input_shape[-1],),
+
+                                 initializer=self.init,
+
+                                 name='{}_W'.format(self.name),
+
+                                 regularizer=self.W_regularizer,
+
+                                 constraint=self.W_constraint)
+
+        if self.bias:
+
+            self.b = self.add_weight((input_shape[1],),
+
+                                     initializer='zero',
+
+                                     name='{}_b'.format(self.name),
+
+                                     regularizer=self.b_regularizer,
+
+                                     constraint=self.b_constraint)
+
         else:
-            raise Exception('Layer could not be build: No information about expected input shape.')
 
-        self.U_a = self.inner_init((self.output_dim, self.output_dim), name='{}_U_a'.format(self.name))
-        self.b_a = keras.backend.zeros((self.output_dim,), name='{}_b_a'.format(self.name))
+            self.b = None
 
-        self.U_m = self.inner_init((attention_dim, self.output_dim), name='{}_U_m'.format(self.name))
-        self.b_m = keras.backend.zeros((self.output_dim,), name='{}_b_m'.format(self.name))
 
-        if self.single_attention_param:
-            self.U_s = self.inner_init((self.output_dim, 1), name='{}_U_s'.format(self.name))
-            self.b_s = keras.backend.zeros((1,), name='{}_b_s'.format(self.name))
+
+        self.built = True
+
+
+
+    def compute_mask(self, input, input_mask=None):
+
+        # do not pass the mask to the next layers
+
+        return None
+
+
+
+    def call(self, x, mask=None):
+
+        eij = dot_product(x, self.W)
+
+
+
+        if self.bias:
+
+            eij += self.b
+
+
+
+        eij = K.tanh(eij)
+
+
+
+        a = K.exp(eij)
+
+
+
+        # apply mask after the exp. will be re-normalized next
+
+        if mask is not None:
+
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+
+            a *= K.cast(mask, K.floatx())
+
+
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+
+        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
+
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+
+
+        weighted_input = x * K.expand_dims(a)
+
+
+
+        result = K.sum(weighted_input, axis=1)
+
+
+
+        if self.return_attention:
+
+            return [result, a]
+
+        return result
+
+
+
+    def compute_output_shape(self, input_shape):
+
+        if self.return_attention:
+
+            return [(input_shape[0], input_shape[-1]),
+
+                    (input_shape[0], input_shape[1])]
+
         else:
-            self.U_s = self.inner_init((self.output_dim, self.output_dim), name='{}_U_s'.format(self.name))
-            self.b_s = keras.backend.zeros((self.output_dim,), name='{}_b_s'.format(self.name))
 
-        self.trainable_weights += [self.U_a, self.U_m, self.U_s, self.b_a, self.b_m, self.b_s]
-
-        if self.initial_weights is not None:
-            self.set_weights(self.initial_weights)
-            del self.initial_weights
-
-    def step(self, x, states):
-        h, [h, c] = super(AttentionLSTM, self).step(x, states)
-        attention = states[4]
-
-        m = self.attn_inner_activation(keras.backend.dot(h, self.U_a) * attention + self.b_a)
-        # Intuitively it makes more sense to use a sigmoid (was getting some NaN problems
-        # which I think might have been caused by the exponential function -> gradients blow up)
-        s = self.attn_activation(keras.backend.dot(m, self.U_s) + self.b_s)
-
-        if self.single_attention_param:
-            h = h * keras.backend.repeat_elements(s, self.output_dim, axis=1)
-        else:
-            h = h * s
-
-        return h, [h, c]
-
-    def get_constants(self, x):
-        constants = super(AttentionLSTM, self).get_constants(x)
-        constants.append(keras.backend.dot(self.attention_vec, self.U_m) + self.b_m)
-        return constants
+            return input_shape[0], input_shape[-1]
 
 #---------------------------------------------------------LSTM----------------------------------------------------------------
 from keras.layers import Dense, Activation, Dropout, Bidirectional, Flatten, RepeatVector, Permute, Lambda, merge
@@ -327,8 +510,9 @@ class VGG16LSTMVideoClassifier(object):
 
     def create_model(self):
         model = Sequential()
-        model.add(Dense(512, activation='relu', input_shape=(None, self.num_input_tokens)))
-        model.add(AttentionLSTM(output_dim=HIDDEN_UNITS, attention_vec=model.input))
+        model.add(LSTM(units=HIDDEN_UNITS, input_shape=(None, self.num_input_tokens), return_sequences=False, dropout=0.5))
+        model.add(Attention())
+        model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(self.nb_classes))
         model.add(Activation('softmax'))
