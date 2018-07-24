ad8aed9bc1025cccc7deae2552b666328c86b1e2
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-07-24 15:22:57 -0500

    Update recurrent_networks.py

--- FallDetectionSystem/model/recurrent_networks.py
+++ FallDetectionSystem/model/recurrent_networks.py
@@ -570,7 +570,7 @@ class VGG16LSTMVideoClassifier(object):
 
     def create_model(self):
         model = Sequential()
-        model.add(AttentionLSTM(LSTM(units=HIDDEN_UNITS, return_sequences=False, dropout=0.5, input_shape=(None, 1, self.num_input_tokens), comsume_less='gpu')
+        model.add(AttentionLSTM(LSTM(units=HIDDEN_UNITS, return_sequences=False, dropout=0.5, input_shape=(None, 1, self.num_input_tokens), comsume_less='gpu')))
         model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(self.nb_classes))
