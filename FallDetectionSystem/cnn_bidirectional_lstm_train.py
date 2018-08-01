5629a5d5d44ee6d3d9a2e42d2066f53f236c8b74
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-08-01 10:03:26 -0500

    Update cnn_bidirectional_lstm_train.py

--- FallDetectionSystem/cnn_bidirectional_lstm_train.py
+++ FallDetectionSystem/cnn_bidirectional_lstm_train.py
@@ -12,9 +12,9 @@ def main():
     K.set_image_dim_ordering('tf')
     sys.path.append(patch_path('..'))
 
-    from keras_video_classifier.library.recurrent_networks import VGG16BidirectionalLSTMVideoClassifier
-    from keras_video_classifier.library.utility.plot_utils import plot_and_save_history
-    from keras_video_classifier.library.utility.ucf.UCF101_loader import load_ucf
+    from model.recurrent_networks import VGG16BidirectionalLSTMVideoClassifier
+    #from keras_video_classifier.library.utility.plot_utils import plot_and_save_history
+    from utility.UCF101_loader import load_ucf
 
     data_set_name = 'UCF-101'
     input_dir_path = patch_path('very_large_data')
@@ -30,8 +30,7 @@ def main():
 
     history = classifier.fit(data_dir_path=input_dir_path, model_dir_path=output_dir_path, data_set_name=data_set_name)
 
-    plot_and_save_history(history, VGG16BidirectionalLSTMVideoClassifier.model_name,
-                          report_dir_path + '/' + VGG16BidirectionalLSTMVideoClassifier.model_name + '-history.png')
+    #plot_and_save_history(history, VGG16BidirectionalLSTMVideoClassifier.model_name, report_dir_path + '/' + VGG16BidirectionalLSTMVideoClassifier.model_name + '-history.png')
 
 
 if __name__ == '__main__':
